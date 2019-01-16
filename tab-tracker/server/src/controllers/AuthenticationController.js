007a3b3e76fa4d7faa7444075a8ee7becafe805a
bjornkraabol <bjorn.kraabol@gmail.com>
2019-01-16 13:21:50 +0100

    Added more models.

--- tab-tracker/server/src/controllers/AuthenticationController.js
+++ tab-tracker/server/src/controllers/AuthenticationController.js
@@ -1,8 +1,16 @@
-module.exports = {
-  register (req, res)  {
-    res.send({
-      message: `Hello ${req.body.email}. Your user was registered! Have fun.`
-    })
+ const {User} = require('../models')
+
+ module.exports = {
+  async register (req, res)  {
+    try{
+      const user = await User.create(req.body)
+      res.send(user.toJSON())
+    } catch (err){
+    	res.status(400).send({
+    		error:'This email is already in use'
+    	})
+    	//email already exists
+    }
    }
  }
 
